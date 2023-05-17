bd3509c7c11cec7bd9772ec8fe7f45243154764f
sdxsd <willmaguire@icloud.com>
2023-05-17 15:31:08 +0200

    Beginning work on the sql script.

---
+++ srcs/requirements/mariadb/conf/base-autoconf.sql
@@ -0,0 +1,11 @@
+-- Creates the LetterCrunch database if it doesn't already exist.
+-- Then specifies that it should be used going forward.
+CREATE DATABASE IF NOT EXISTS LetterCrunch;
+USE LetterCrunch;
+
+-- The Dockerfile will substitute PASSWD and USRNAME with data provided by the .env file.
+CREATE USER IF NOT EXISTS 'USRNAME'@'localhost' IDENTIFIED BY 'PASSWD';
+-- Grants all privileges: SELECT, INSERT, DELETE, DROP, etc.. to the user created above.
+GRANT ALL PRIVILEGES ON LetterCrunch.* TO 'USRNAME'@'localhost';
+-- Applies the granting of privilege.
+FLUSH PRIVILEGES;
