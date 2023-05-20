074f4e66babe4a0f0b0cfccefbb9d57facb824cd
sdxsd <willmaguire@icloud.com>
2023-05-20 21:02:14 +0200

    Still can't connect. Why?

--- srcs/requirements/mariadb/conf/base-autoconf.sql
+++ srcs/requirements/mariadb/conf/base-autoconf.sql
@@ -1,16 +1,17 @@
 -- Creates the LetterCrunch database if it doesn't already exist.
 -- Then specifies that it should be used going forward.
-CREATE DATABASE IF NOT EXISTS LetterCrunch;
-USE LetterCrunch;
+CREATE DATABASE IF NOT EXISTS lettercrunch;
+USE lettercrunch;
+
 
 -- The Dockerfile will substitute PASSWD and USRNAME with data provided by the .env file.
-CREATE USER IF NOT EXISTS 'USRNAME'@'localhost' IDENTIFIED BY 'PASSWD';
+CREATE USER IF NOT EXISTS 'USRNAME'@'%' IDENTIFIED BY 'PASSWD';
 -- Grants all privileges: SELECT, INSERT, DELETE, DROP, etc.. to the user created above.
-GRANT ALL PRIVILEGES ON LetterCrunch.* TO 'USRNAME'@'localhost';
+GRANT ALL PRIVILEGES ON lettercrunch.* TO 'USRNAME'@'%';
 -- Applies the granting of privilege.
 FLUSH PRIVILEGES;
 
 -- Creating the regular user.
-CREATE USER IF NOT EXISTS 'REGUSRNAME'@'localhost' IDENTIFIED BY 'REGPASSWD';
-GRANT SELECT ON LetterCrunch.* TO 'REGUSRNAME'@'localhost';
+CREATE USER IF NOT EXISTS 'REGUSRNAME'@'%' IDENTIFIED BY 'REGPASSWD';
+GRANT SELECT ON lettercrunch.* TO 'REGUSRNAME'@'%';
 FLUSH PRIVILEGES;
